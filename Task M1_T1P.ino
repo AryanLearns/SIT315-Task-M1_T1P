62f60e9f17c8726f4cc8f5f801314d5f3bfc2b47
Aryan Singh <121689701+AryanLearns@users.noreply.github.com>
2024-01-31 10:03:59 +0530

    Add files via upload

---
+++ Task M1_T1P.ino
@@ -0,0 +1,23 @@
+const int pirSensorPin = 2;  // PIR sensor to digital pin 2
+const int ledPin = 5;      // LED connected to digital pin 13
+
+void setup() {
+  pinMode(pirSensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int motionDetected = digitalRead(pirSensorPin);
+
+  if (motionDetected == HIGH) {
+    digitalWrite(ledPin, HIGH);  // Turn on the LED
+    Serial.println("Motion detected!");
+    delay(1000);  
+  } else {
+    Serial.println("No motion detected!");
+    digitalWrite(ledPin, LOW);  // Turn off the LED
+  }
+  
+  delay(1000);
+}
